f1ad6260a3f7aa4489f29b8093b86618f1756a3f
shaham7 <mohd.shaham7@gmail.com>
2024-12-30 22:16:40 +0400

    prophet prediction and metrix calculation

--- scripts/analysis_and_forecasting.R
+++ scripts/analysis_and_forecasting.R
@@ -29,3 +29,40 @@ lines(ma_5, col = "blue")
 
 arima_model <- auto.arima(ts_data, seasonal = FALSE, stepwise = TRUE, approximation = FALSE, trace = TRUE)
 summary(arima_model)
+
+forecast_periods <- 10
+arima_forecast <- forecast(arima_model, h = forecast_periods)
+plot(arima_forecast)
+
+prophet_data <- data.frame(
+  ds = as.Date(paste0(country_data$Year, "-01-01")),
+  y = country_data$Emissions
+)
+head(prophet_data)
+
+prophet_model <- prophet(prophet_data)
+
+future_dates <- make_future_dataframe(prophet_model, periods = forecast_periods, freq = "year")
+future_dates
+
+prophet_forecast <- predict(prophet_model, future_dates)
+
+plot(prophet_model, prophet_forecast)
+
+prophet_forecast
+
+#creating a new forcast to try 
+
+
+actual <- tail(prophet_data$y, forecast_periods)
+
+predicted <- tail(prophet_forecast$yhat, forecast_periods)
+predicted
+
+calculate_forecast_metrics <- function(actual, predicted) {
+  rmse <- sqrt(mean((actual - predicted)^2))
+  mae <- mean(abs(actual - predicted))
+  mape <- mean(abs((actual - predicted)/actual)) * 100
+  return(data.frame(RMSE = rmse, MAE = mae, MAPE = mape))
+}
+calculate_forecast_metrics(actual, predicted)
